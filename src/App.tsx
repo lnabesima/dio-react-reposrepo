aa7a523521d2401f8b76ec7d8d278ca222ed34e2
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-04-11 13:11:29 -0300

    feat: enhance app layout and add GitHub logo
    Added a GitHub logo to the app and introduced a FormContainer component
    for future use. Updated global styles to include padding, centering, and
     full viewport height for better layout structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import GithubLogo from './assets/github.svg';
+import { FormContainer } from './components/FormContainer';
+
 export default function App(){
   return (
     <>
       <p>Hello World</p>
+      <img src={GithubLogo} alt="Github Logo" />
     </>
   )
 }
--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -10,5 +10,10 @@ export const GlobalStyle = createGlobalStyle`
         background-color: #151b23;
         color: #d1d7e0;
         font-family: "Segoe UI", "Arial", "Helvetica", sans-serif;
+        padding: 0.625rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 100dvh;
     }
 `
