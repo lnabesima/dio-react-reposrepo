81713bfa2cddb647928897d0871c4454137b45ab
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-04-11 14:38:39 -0300

    refactor: centralize and reuse CSS variables for theming
    This commit creates a `variables.css` file to store shared CSS variables
     and updates `globalStyles.ts` to reference these variables. This
     improves maintainability and ensures consistent theming across the
     application.

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,14 +1,26 @@
 import {createGlobalStyle} from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+    :root {
+        --bg-color: #151b23;
+        --gh-blue: #316dca;
+        --gh-text: #d1d7e0;
+        --gh-text-faded: #9198a1;
+        --gh-card: #212830;
+        --gh-accent: #262c36;
+        --gh-green: #3b8640;
+        --gh-star: #daaa3f;
+    }
+
     *, body {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
     }
+
     body {
-        background-color: #151b23;
-        color: #d1d7e0;
+        background-color: var(--bg-color);
+        color: var(--gh-text);
         font-family: "Segoe UI", "Arial", "Helvetica", sans-serif;
         padding: 0.625rem;
         display: flex;
