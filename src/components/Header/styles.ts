b00093f63fa7d5d0dc64ea8dbfc32dbde1938f3a
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-04-18 16:33:17 -0300

    feat: add Header and update RepoList with title
    Introduce a new Header component with a styled layout and integrate it
    into the App. Update RepoList to include a title for better clarity.
    These changes enhance the UI and improve the user experience.

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledHeaderContainer = styled.header`
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+    margin-block: 0.625rem;
+    padding-inline: 0.625rem;
+    align-items: center;
+
+& img {
+    height: 8rem;
+    width: 8rem;
+};
+`
